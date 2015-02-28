53f28336c1359e84afc10295db37fea1190c472f
Jeremy Rabasco <jeremy.rabasco@epfl.ch>
2015-02-28 18:53:29 +0100

    Error handling

--- upit.py
+++ upit.py
@@ -1,36 +1,53 @@
 import requests
+import requests.exceptions
 import json
 import sys
 import base64
 from subprocess import Popen, PIPE
 
 
-def to_clipboard(text):
+def to_clipboard(text: str):
     clipboard_proc = Popen(['xclip', '-selection', 'c'], stdin=PIPE)
     clipboard_proc.communicate(input=bytes(text, 'utf-8'))
 
-api_key = "YOUR_API_KEY"
- 
-url = r"https://api.imgur.com/3/upload.json"
- 
-image_path = sys.argv[1]
- 
-f = open(image_path, 'rb')
 
-binary_data = f.read()
-b64image = base64.b64encode(binary_data)
+# replace with any notification logic you like
+def notify(notif_type: str, title: str, msg: str):
+    notif_proc = Popen(["notify-send", "-i", notif_type,  title, msg])
+    notif_proc.communicate()
 
-title = "Screeny"
 
-payload = {"key": api_key,
-           "image": b64image,
-           "title": title}
+def notify_info(title: str, msg: str):
+    notify("info", title, msg)
 
-req_result = requests.post(url, data=payload, headers={'Authorization': "Client-ID YOUR_CLIENT_ID"})
- 
-result_json = json.loads(req_result.text)
 
-to_clipboard(result_json["data"]["link"])
-# Replace this with any notification logic you'd want to have
-notif_proc = Popen(["notify-send", "-i", "info",  "Screenshot uploaded", "URL has been copied to clipboard"])
-notif_proc.communicate()
+def notify_error(title: str, msg: str):
+    notify("error", title, msg)
+
+
+API_KEY = "YOUR_API_KEY"
+URL = "https://api.imgur.com/3/upload.json"
+TITLE = "Screenshot"
+
+
+with open(sys.argv[1], 'rb') as image:
+    b64_image = base64.b64encode(image.read())
+
+payload = {"key": API_KEY,
+           "image": b64_image,
+           "title": TITLE}
+
+req_result = None
+try:
+    req_result = requests.post(URL, data=payload, headers={'Authorization': "Client-ID YOUR_CLIENT_ID"})
+except requests.exceptions.ConnectionError:
+    notify_error("Connection Error", "Verify your internet connection")
+
+if req_result is not None:
+    result_json = json.loads(req_result.text)
+
+    try:
+        to_clipboard(result_json["data"]["link"])
+        notify_info("Upload succeeded", "The URL of your upload has been placed in your clipboard.")
+    except KeyError:
+        notify_error("Request malformed", "Imgur answered \"" + result_json["data"]["error"] + "\"")
